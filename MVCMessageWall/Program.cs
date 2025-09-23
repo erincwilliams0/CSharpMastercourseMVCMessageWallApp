e40f0c88141016d3b786f63679c0dfd79a53b2bd
erin williams <erin.cwilliams0@gmail.com>
2025-09-22 21:20:44 -0500

    feat: Finished Lesson

--- MVCMessageWall/Program.cs
+++ MVCMessageWall/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MVCMessageWall.Areas.Identity.Data;
+using MVCMessageWall.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MVCMessageWallContextConnection") ?? throw new InvalidOperationException("Connection string 'MVCMessageWallContextConnection' not found.");;
+
+builder.Services.AddDbContext<MVCMessageWallContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<MVCMessageWallUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<MVCMessageWallContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -14,6 +23,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
 app.UseRouting();
 
 app.UseAuthorization();
@@ -24,6 +35,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
+app.MapRazorPages();
 
 app.Run();
